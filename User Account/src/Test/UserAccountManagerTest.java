e666bf408a5e14627c06c79d8312669cd859e563
Alex Esplin <alexr.esplin@gmail.com>
2018-09-12 00:26:21 -0600

    Created more Tests for UserAccount and UserAccountManager

--- User Account/src/Test/UserAccountManagerTest.java
+++ User Account/src/Test/UserAccountManagerTest.java
@@ -3,27 +3,82 @@ package Test;
 
 import Production.UserAccountManager;
 import junit.framework.TestCase;
-
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class UserAccountManagerTest extends TestCase {
 
 	private UserAccountManager userAccountManager;
-	
+
+	@Before
 	protected void setUp() throws Exception {
 		super.setUp();
 		userAccountManager = new UserAccountManager();
-		userAccountManager.addUserAccount("admin", "123456", "Kevin Brown",
+		userAccountManager.addUserAccount("admin", "123456", "Alex Esplin",
 				"alexesplin1@u.boisestate.edu", "Chicken");
+		userAccountManager.addUserAccount("phin", "secretpassword", "Bob",
+				"bob@gmail.com", "hotdogs");
 	}
 
+	@Test
 	public void testExistingUserAccount() {
+		assertNotNull(userAccountManager);
 		assertTrue(userAccountManager.doesUserNameExist("admin"));
 		assertTrue(userAccountManager.doesAccountExist("admin", "123456"));
+		assertTrue(userAccountManager.doesUserNameExist("phin"));
+		assertTrue(userAccountManager.doesAccountExist("phin", "secretpassword"));
 	}
 
+	@Test
 	public void testNonExistantUserAccount() {
+		assertNotNull(userAccountManager);
 		assertFalse(userAccountManager.doesUserNameExist("BSU"));
+		assertFalse(userAccountManager.doesAccountExist("BSU", "hello"));
+	}
+
+	@Test
+	public void testRightUsernameWrongPassword(){
+		assertNotNull(userAccountManager);
+		assertTrue(userAccountManager.doesUserNameExist("admin"));
 		assertFalse(userAccountManager.doesAccountExist("admin", "hello"));
 	}
 
+	@Test
+	public void testWrongUsernameRightPassword(){
+		assertNotNull(userAccountManager);
+		assertFalse(userAccountManager.doesUserNameExist("coolguy"));
+		assertFalse(userAccountManager.doesAccountExist("coolguy", "123456"));
+	}
+
+	@Test
+	public void testPasswordValidity(){
+		assertNotNull(userAccountManager);
+		assertFalse(userAccountManager.validPassword(""));
+		assertFalse(userAccountManager.validPassword("jjjjjjjjjjjjjjjjjjjjjjjj"));
+		assertTrue(userAccountManager.validPassword("123456"));
+		assertTrue(userAccountManager.validPassword("hunter2"));
+	}
+
+	@Test
+	public void testEmailValidity(){
+		assertNotNull(userAccountManager);
+		assertFalse(userAccountManager.validEmail(""));
+		assertFalse(userAccountManager.validEmail("thisIsMyEmail"));
+		assertTrue(userAccountManager.validEmail("someguy@gmail.com"));
+	}
+
+	@Test
+	public void testSecurityQuestionValidity(){
+		assertNotNull(userAccountManager);
+		assertFalse(userAccountManager.validSecurityQuestion(""));
+		assertTrue(userAccountManager.validSecurityQuestion("Chicken"));
+		assertTrue(userAccountManager.validSecurityQuestion("burger and fries"));
+	}
+
+	@After
+	public void tearDown() throws Exception{
+		super.tearDown();
+		userAccountManager = null;
+	}
 }
--- User Account/src/Test/UserAccountTest.java
+++ User Account/src/Test/UserAccountTest.java
@@ -3,17 +3,57 @@ package Test;
 
 import Production.UserAccount;
 import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-public class UserAccountTest extends TestCase {
+public class UserAccountTest  extends TestCase {
 
 	private UserAccount userAccount;
-	
-	public void testNewAccount() {
-		userAccount = new UserAccount("admin", "123456", "Kevin Brown",
-				"alexesplin1@u.boisestate.edu", "Chicken");
+
+	@Before
+	protected void setUp() throws Exception {
+		super.setUp();
+			userAccount = new UserAccount("admin", "123456", "Alex Esplin",
+					"alexesplin1@u.boisestate.edu", "Chicken");
+	}
+
+	@Test
+	public void testNewAccount(){
+		assertNotNull(userAccount);
+		assertEquals("admin", userAccount.getUserName());
+		assertEquals("123456", userAccount.getPassword());
+		assertEquals("Alex Esplin", userAccount.getName());
+		assertEquals("alexesplin1@u.boisestate.edu", userAccount.getEmail());
+		assertEquals("Chicken", userAccount.getSecurityQuestion());
+	}
+
+	@Test
+	public void testValidCredential() {
 		assertNotNull(userAccount);
 		assertTrue(userAccount.matchUserName("admin"));
 		assertTrue(userAccount.isValidCredential("admin", "123456"));
 	}
 
+	@Test
+	public void testChangeAccount(){
+		assertNotNull(userAccount);
+		userAccount.setUserName("coolguy");
+		assertEquals("coolguy", userAccount.getUserName());
+		userAccount.setPassword("hunter2");
+		assertEquals("hunter2", userAccount.getPassword());
+		userAccount.setEmail("someguy@gmail.com");
+		assertEquals("someguy@gmail.com", userAccount.getEmail());
+		userAccount.setName("Kevin");
+		assertEquals("Kevin", userAccount.getName());
+		userAccount.setSecurityQuestion("Beef");
+		assertEquals("Beef", userAccount.getSecurityQuestion());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		super.tearDown();
+		userAccount = null;
+		assertNull(userAccount);
+	}
 }
